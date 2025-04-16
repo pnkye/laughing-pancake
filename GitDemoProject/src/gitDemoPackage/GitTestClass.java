a576cc4bac92d3291d387ad175b987e2d4f83641
quetsss <quetsss@10.1.51.24>
2025-04-16 10:07:42 -0700

    First commit

---
+++ GitDemoProject/src/gitDemoPackage/GitTestClass.java
@@ -0,0 +1,11 @@
+package gitDemoPackage;
+
+public class GitTestClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is Get Demo project");
+		System.out.println("created by me");
+
+	}
+
+}
