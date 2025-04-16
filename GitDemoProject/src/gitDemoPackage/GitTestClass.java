c33f12ac562454051aa7bb67b359770317ae7fbb
300398754 <300398754@N6105S02.ad.douglascollege.ca>
2025-04-16 10:45:56 -0700

    test

--- GitDemoProject/src/gitDemoPackage/GitTestClass.java
+++ GitDemoProject/src/gitDemoPackage/GitTestClass.java
@@ -5,6 +5,10 @@ public class GitTestClass {
 	public static void main(String[] args) {
 		System.out.println("This is Get Demo project");
 		System.out.println("created by me");
+		System.out.println("Hi its me");
+		System.out.println("Hi member");
+
+
 
 	}
 
